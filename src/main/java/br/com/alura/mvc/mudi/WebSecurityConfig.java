0d930db985dbbf2c0a2f1473d332703d8e1c57bb
Matheus Braz <matheus.braz@turn2c.com>
2022-08-08 18:15:08 -0300

    Autenticação com Spring Security

---
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package br.com.alura.mvc.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        ).formLogin(
+                form -> form.loginPage("/login").permitAll()
+        ).logout(logout -> logout.logoutUrl("/logout"));
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("ADM").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
